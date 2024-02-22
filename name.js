9c6c0ce6add3a7e76799574b528d42b0d17b07a0
MuhammadAyan67 <159698909+MuhammadAyan67@users.noreply.github.com>
2024-02-22 13:08:37 +0500

    Add files via upload

---
+++ name.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "Muhammad Ayaan";
+console.log("lowercase:", personName.toLowerCase());
+console.log("uppercase:", personName.toUpperCase());
+console.log("titlecase:", personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(''));
---
+++ name.ts
@@ -0,0 +1,4 @@
+let personName:string= "Muhammad Ayaan"
+console.log("lowercase:" ,personName.toLowerCase())
+console.log("uppercase:",personName.toUpperCase())
+console.log("titlecase:",personName.split(' ') .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(''))
